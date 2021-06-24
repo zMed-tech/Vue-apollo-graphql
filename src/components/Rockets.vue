ad39e3dedd39e3a93bc6507a1d21bc9bd302df33
zMed-tech <badache.amn@gmail.com>
2021-06-24 15:41:49 +0100

    display all rockets on rockets page

---
+++ src/components/Rockets.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="apol">
+    <h1>Apollo GraphQl</h1>
+
+    <ul>
+      <li v-for="rocket in rockets" :key="rocket.id">
+        <router-link :to="{ name: 'Rocket', params: { id: rocket.id } }">
+          🗺 {{ rocket.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+const query = gql`
+  query getRockets {
+    rockets {
+      id
+      name
+    }
+  }
+`;
+export default {
+  apollo: {
+    rockets: {
+      query,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#apol {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
