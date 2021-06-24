150053fac87990c0516950e404f6596fdaf4b868
zMed-tech <badache.amn@gmail.com>
2021-06-24 15:42:02 +0100

    import rocket page to router

--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,12 @@ const routes = [
     name: "Home",
     component: Home,
   },
+
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/rocket/:id",
+    name: "Rocket",
+    props: true,
+    component: () => import("@/components/Rocket.vue"),
   },
 ];
 
