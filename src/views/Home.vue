57484ea26417fbce3c2c660de0e33033323cc3c9
zMed-tech <badache.amn@gmail.com>
2021-06-24 15:42:15 +0100

    import rockets page to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Rockets />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Rockets from "@/components/Rockets.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Rockets,
   },
 };
 </script>
